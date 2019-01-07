cac6e5cdbd4a92450ff95e9227c8f31cf3f28cb0
JustBrenkman <JustBrenkman@gmail.com>
2019-01-07 13:22:18 -0700

    Initial commit

---
+++ src/com/cs240/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.cs240.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("I love Java");
+    }
+}
